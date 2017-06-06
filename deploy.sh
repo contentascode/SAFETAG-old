02ba8c2ba6a8848c8570d6de85e71ceb34d5f244
Jun Matsushita <jun@iilab.org>
2017-06-06 22:33:14 +0200

    Fix deploy script paths

--- deploy.sh
+++ deploy.sh
@@ -22,8 +22,8 @@ SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
-cd out
+git clone $REPO build
+cd build
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
